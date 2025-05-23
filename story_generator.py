4c63a0e855fd76df2fd239475c9e1322032ce16a
Thrishaa J <thrishaa.j29@gmail.com>
2025-05-23 17:15:07 +0530

    Update story_generator.py

--- story_generator.py
+++ story_generator.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 from huggingface_hub import InferenceClient
 
 # ✅ Load your Hugging Face key from .env
-load_dotenv()
-hf_token = os.getenv("HF_API_KEY")
+import streamlit as st
+hf_token = st.secrets["HF_API_KEY"]
+
 
 # ✅ Initialize Hugging Face Nebius Inference Client
 client = InferenceClient(
